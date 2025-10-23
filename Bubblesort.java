3d2a2808564d2e404e6519b95d1d3ed289fcb61f
SAURABH KUMAR <147742073+skkushwah03@users.noreply.github.com>
2025-10-24 00:57:00 +0530

    Bubblesort.java
    Implement Bubblesort algorithm to sort an array.

---
+++ Bubblesort.java
@@ -0,0 +1,18 @@
+package _7_Sorting;
+public class Bubblesort {
+    public static void main(String []args){
+        int arr[]={1,5,3,2,7};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
