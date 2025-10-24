6d463c68748bdb90946f8621b393de8964a4c5aa
SAURABH KUMAR <147742073+skkushwah03@users.noreply.github.com>
2025-10-25 00:20:41 +0530

    Create CountingSort.java

---
+++ CountingSort.java
@@ -0,0 +1,24 @@
+package _7_Sorting;
+
+public class CountingSort {
+    public static void main(String[] args) {
+        int arr[]={1,4,1,3,2,4,3,7};
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            largest=Math.max(largest, arr[i]);
+        }
+        int count[]=new int[largest+1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        int j=0;
+        for(int i=0;i<arr.length;i++){
+            while (count[i]>0) {
+                arr[j]=i;
+                j++;
+                count[i]--;
+                
+            }
+        }
+    }
+}
