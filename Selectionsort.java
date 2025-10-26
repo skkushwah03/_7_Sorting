ac8c6f4f48168469bdc734e8b013a03bb9beeb87
SAURABH KUMAR <147742073+skkushwah03@users.noreply.github.com>
2025-10-26 08:42:14 +0530

    Create Selectionsort.java

---
+++ Selectionsort.java
@@ -0,0 +1,21 @@
+package _7_Sorting;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = {64, 25, 12, 22, 11};
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
